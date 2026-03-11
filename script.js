dfb1c5e1fed5ab6ced420fba96bdd6e2d582e1ed
Prateek Wats <prateekwats21@gmail.com>
2026-03-11 23:16:48 +0530

    Added portfolio website

---
+++ script.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+
+sections.forEach(section => {
+
+const position = section.getBoundingClientRect().top;
+
+const screenPosition = window.innerHeight / 1.3;
+
+if(position < screenPosition){
+section.style.opacity = 1;
+section.style.transform = "translateY(0)";
+}
+
+});
+
+});
